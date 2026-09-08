12ed1855fc7bab4a59041f46748515eb12900773
reneradoi <rene.radoi@canonical.com>
2026-09-08 16:45:04 +0200

    wait for Valkey to settle after CA cert relation was established

--- tests/integration/clients/test_ldap_authentik.py
+++ tests/integration/clients/test_ldap_authentik.py
@@ -159,6 +159,11 @@ def test_ldap_integration(
 
     logger.info("Add LDAP CA certificate")
     juju.integrate(f"{APP_NAME}:ldap-ca-cert", ca_name)
+    # wait for the CA cert relation to settle
+    juju.wait(
+        lambda status: are_agents_idle(status, APP_NAME, idle_period=30, unit_count=NUM_UNITS),
+        timeout=600,
+    )
     juju.wait(
         lambda status: does_status_match(
             status,
